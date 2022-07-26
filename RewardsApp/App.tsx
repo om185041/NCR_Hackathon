93985fc1b9281d62d0344ae3ecc73de0620946cb
Brenda Cano <109369061+bc185194@users.noreply.github.com>
2022-07-25 20:44:55 -0400

    example page

--- RewardsApp/App.tsx
+++ RewardsApp/App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ExampleScreen from './app/screens/ExampleScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* Below I just added the example screen from RewardsApp/app/screens/ExampleScreen */}
+      < ExampleScreen ></ExampleScreen>
     </View>
   );
 }
